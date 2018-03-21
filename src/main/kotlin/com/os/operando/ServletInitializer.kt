c773bc74e5da89323217776b522988da397b578d
Shinobu Okano <okanoshinobu8@gmail.com>
2018-03-21 12:46:59 +0900

    📦

--- src/main/kotlin/com/os/operando/ServletInitializer.kt
+++ src/main/kotlin/com/os/operando/ServletInitializer.kt
@@ -1,7 +1,7 @@
 package com.os.operando
 
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 open class ServletInitializer : SpringBootServletInitializer() {
 
